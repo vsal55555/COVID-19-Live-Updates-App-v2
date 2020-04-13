b086906235b89148ac840eb98613057096a260a1
Bishal Shrestha <vsal55555@gmail.com>
2020-04-13 03:07:35 -0700

    Initial Commit

---
+++ Project_1_shree_Covid-19-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project_1_shree_Covid-19-demo
+//
+//  Created by BSAL-MAC on 4/13/20.
+//  Copyright © 2020 BSAL-MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
