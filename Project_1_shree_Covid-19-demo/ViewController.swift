1ff3d2779f03e1fbda651cdca765b6cc62d5abe8
Bishal Shrestha <vsal55555@gmail.com>
2020-04-13 11:24:38 -0700

    COVID-19 Live Updates App
    Want to stay informed with all the latest information surrounding COVID-19.

--- Project_1_shree_Covid-19-demo/ViewController.swift
+++ Project_1_shree_Covid-19-demo/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+var name = ["Where to call for help","About COVID-19","Prevention","If you  get sick","Travel Advice"]
 
+    @IBOutlet weak var QAtableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -17,4 +20,19 @@ class ViewController: UIViewController {
 
 
 }
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    //needed 2 methods to conform tableview controller delegate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return name.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? cellTableViewCell
+//cast this cell to tableviewcell so xcode knows
+        //now we've to pass array data to this new cell
+        
+        return cell!
+    }
+}
+
 
---
+++ Project_1_shree_Covid-19-demo/cellTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  cellTableViewCell.swift
+//  Project_1_shree_Covid-19-demo
+//
+//  Created by BSAL-MAC on 4/13/20.
+//  Copyright © 2020 BSAL-MAC. All rights reserved.
+//
+
+import UIKit
+
+class cellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cellImg: UIImageView!
+    @IBOutlet weak var cellLbl: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
